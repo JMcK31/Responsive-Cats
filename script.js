d50d35a2cea3e8fdb00fa0af94b99a573fe49420
Jesse McKenzie <jfmckenz31@gmail.com>
2018-09-12 22:56:42 -0400

    added audio file, attempted to link audio play to key sequence event

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ $('#myModal').on('shown.bs.modal', function () {
 
 const pressed = [];
 const secretCode = 'meow'
-const audio = audio.play('http://dev.interactive-creation-works.net/1/1.ogg');
+const audio = document.getElementById('myAudio');
 window.addEventListener('keyup', (e) => {
     console.log(e.key);
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
 
     if (pressed.join('').includes(secretCode)) {
-        console.log('Ding Ding!');
         audio.play();
     }
     console.log(pressed);
