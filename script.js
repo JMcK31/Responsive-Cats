4bf8326dcb57c6f8b914d9ef16dc064d3597116e
Jesse McKenzie <jfmckenz31@gmail.com>
2018-09-12 22:48:49 -0400

    attempt to add easter egg key sequence meow, js added but not working

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
 })
 
-$('#myModal').modal('toggle')
+
+const pressed = [];
+const secretCode = 'meow'
+const audio = audio.play('http://dev.interactive-creation-works.net/1/1.ogg');
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+    if (pressed.join('').includes(secretCode)) {
+        console.log('Ding Ding!');
+        audio.play();
+    }
+    console.log(pressed);
+});
